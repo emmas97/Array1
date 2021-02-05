d9f28bd698d9bc0af7f88a67d8165f969faf7678
Emma Schaffer <emmaschaffer@usf.edu>
2021-02-05 14:33:45 -0500

    Add project files.

---
+++ Array1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Array1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[25];
+            for (int i = 0; i < 26; i++)
+            {
+                number[i] = i + 1;
+                Console.WriteLine("Element Value =" + number[i]);
+            }
+        }
+    }
+}
